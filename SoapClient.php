dfb4b0b108ed4b5031afa722343ce9340860cbc1
Matthew Hayes <matthew@allthingscode.com>
2012-12-10 16:50:12 -0600

    Sample Code

--- SoapClient.php
+++ SoapClient.php
@@ -1,3 +1,59 @@
 <?php
 
-    // @TODO
+/**
+ * @package Misc
+ */
+class Misc_SoapClient
+{
+
+
+    public static function getDataFromRemoteService()
+    {
+        /*
+        <xs:Return>
+            <xs:Company>
+                <xs1:Name>
+                    <xs1:Short>ATC</xs1:Short>
+                    <xs1:Long>All Things Code</xs1:Long>
+                </xs1:Name>
+            </xs:Company>
+
+            <xs:Customer>
+                <xs2:Name>
+                    <xs2:First>John</xs2:First>
+                    <xs2:Middle>C</xs2:Middle>
+                    <xs2:Last>Doe</xs2:Last>
+                </xs2:Name>
+            </xs:Customer>
+        </xs:Return>
+        */
+
+        // The service
+        //   contains multiple entities with the same name, but different namespaces.
+        //   The PHP SoapClient library is not able to differentiate between
+        //   multiple entities with the same name and different namespaces.
+
+        $classMap = array(
+            'Return'   => 'Misc_SoapClient_ValueObjects_Return',
+            'Company'  => 'Misc_SoapClient_ValueObjects_Return_Company',
+            'Name'     => 'Misc_SoapClient_ValueObjects_Return_Company_Name',
+            'Customer' => 'Misc_SoapClient_ValueObjects_Return_Customer',
+            'Name'     => 'Misc_SoapClient_ValueObjects_Return_Customer_Name'
+            );
+        $soapClientOptions = array(
+            'classmap'       => $classMap,
+            'location'       => 'wsdl.xml',
+            'keep_alive'     => false,
+            'exceptions'     => false,
+            'trace'          => 1,      // allows access to request and response xml
+            'cache_wsdl'     => WSDL_CACHE_NONE
+            );
+        $mySoapClient = new SoapClient( 'wsdl.xml', $soapClientOptions );
+
+        // Returns a "xs:Return" element
+        $retrievedData = $mySoapClient->getData();
+
+
+        // @TODO Do some stuff with our retrieved data
+    }
+}
